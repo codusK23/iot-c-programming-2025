e2f3e2d737766bc16c7c7478305dc558d31980a3
codusK23 <codusK23@gmail.com>
2025-03-18 17:45:22 +0900

    day06 commit

--- day06_C++/st_c.cpp
+++ day06_C++/st_c.cpp
@@ -1,5 +1,5 @@
 /*
-	C ±¸Á¶Ã¼
+	C êµ¬ì¡°ì²´
 */
 #include <iostream>
 
@@ -14,7 +14,7 @@ void showHuman(Human ah) {
 }
 
 int main() {
-	Human h = { "±èÃ¶¼ö", 30, "¹é¼ö" };
+	Human h = { "ê¹€ì² ìˆ˜", 30, "ë°±ìˆ˜" };
 	showHuman(h);
 
 	return 0;
