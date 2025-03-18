2e1a9c2aa1ef2294c5d73a9a1f8e6bc0d83dc0c5
codusK23 <codusK23@gmail.com>
2025-03-18 18:02:00 +0900

    day05 commit

--- day05/voidpointer.c
+++ day05/voidpointer.c
@@ -4,6 +4,7 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 
 int sum(int, int);
 int min(int, int);
@@ -14,13 +15,8 @@ int main()
 {
 	int (*fp)(int, int);		// 함수 포인터
 	int res;
-	//res = sum(20, 10);
-	//res = min(20, 10);
-	//fp = sum;
-	//res = fp(20, 10);
-	//printf("res: %d\n", res);
 
-	(*func)(10, 20);
+	func(&fp);
 
 	return 0;
 }
@@ -39,19 +35,27 @@ int min(int a, int b)
 
 void func(int (*fp)(int, int)) 
 {
-	char a[4];
-	printf("연산하고자 하는 함수를 입력해주세요: ");
-	scanf("%s", a);
-	printf("%s", a);
+	char a[10];
+	int x;
+	int y;
 
-	if (a == 'sum') {
-		printf("더하기");
+	printf("연산하고자 하는 함수를 입력해주세요 (sum/min): ");
+	scanf_s("%s", a);
+
+	if (strcmp(a, 'sum') == 0) {
+		fp = sum;
+	}
+	else if (strcmp(a, 'min') == 0) {
+		fp = min;
+	}
+	else {
+		printf("잘못된 입력입니다.\n");
 	}
 
-	//fp = sum;
-	//int res = fp(20, 10);
-	
-	//printf("원하는 함수가 무엇입니까?");
-	// if문이나 switch문
+	printf("연산하고자 하는 정수 두 개를 입력해주세요: ");
+	scanf_s("%d %d", &x, &y);
+
+	int res = fp(x, y);
+	printf("연산 결과: ", res);
 	
 }
