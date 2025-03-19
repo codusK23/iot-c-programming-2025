cdb1aa878ae90388075bcaed28f70379cb805416
codusK23 <codusK23@gmail.com>
2025-03-19 17:52:32 +0900

    day05 commit

--- day05/voidpointer.c
+++ day05/voidpointer.c
@@ -4,7 +4,6 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
 
 int sum(int, int);
 int min(int, int);
@@ -13,10 +12,10 @@ void func(int (*fp)(int, int));
 //void func()
 int main()
 {
-	int (*fp)(int, int);		// 함수 포인터
+	int (*fp)(int, int) = NULL;		// 함수 포인터
 	int res;
 
-	func(&fp);
+	func(fp);
 
 	return 0;
 }
@@ -40,22 +39,23 @@ void func(int (*fp)(int, int))
 	int y;
 
 	printf("연산하고자 하는 함수를 입력해주세요 (sum/min): ");
-	scanf_s("%s", a);
+	scanf("%s", a);
 
-	if (strcmp(a, 'sum') == 0) {
+	if (strcmp(a, "sum") == 0) {
 		fp = sum;
 	}
-	else if (strcmp(a, 'min') == 0) {
+	else if (strcmp(a, "min") == 0) {
 		fp = min;
 	}
 	else {
 		printf("잘못된 입력입니다.\n");
+		return;
 	}
 
 	printf("연산하고자 하는 정수 두 개를 입력해주세요: ");
 	scanf_s("%d %d", &x, &y);
 
 	int res = fp(x, y);
-	printf("연산 결과: ", res);
+	printf("연산 결과: %d", res);
 	
 }
